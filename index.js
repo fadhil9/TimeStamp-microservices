b9f0eb2292405bb205df500cffd7ac4e7feeb30d
fadhil9 <fadhilmuhamad272@gmail.com>
2024-07-24 14:33:13 +0700

    update

--- index.js
+++ index.js
@@ -24,6 +24,53 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/2015-12-25",(req,res)=>{
+  let time = "2015-12-25";
+  console.log(time);
+
+  let unix = new Date(time).getTime()
+
+  time = new Date(time).toUTCString()
+
+  res.json({unix:unix,utc:time})
+})
+
+app.get("/api/1451001600000",(req,res)=>{
+  let time = 1451001600000;
+  time.toString()
+  let unix = new Date(time).getTime()
+  time = new Date(time).toUTCString()
+
+  res.json({unix:unix,utc:time})
+})
+
+app.get("/api/:date",(req,res)=>{
+  let {date} = req.params
+  let unixx;
+  let times;
+  //isnNaN() itu bernilai false jika  bernilai nomor atau yg bisa dikonversi kenomor
+  //cara konversi ke unix itu dengan getTime dan outputnya nomor/yg bisa dikonversi kenomor
+if(!isNaN(date)){
+  unixx = new Date(parseInt(date)).getTime();
+  times = new Date(parseInt(date)).toUTCString();
+}else{
+  if(isNaN(new Date(date).getTime())){  //jika ternyata date dikonversi ke unix hasilnya bukan nomer, maka error
+    res.json({error : "Invalid Date"})
+  }else{
+    unixx = new Date(date).getTime();
+    times = new Date(date).toUTCString()
+  }
+}
+res.json({ unix: unixx, utc: times });
+})
+
+app.get("/api/",(req,res)=>{
+  unixx = new Date().getTime();
+  times = new Date().toUTCString()
+  
+  res.json({ unix: unixx, utc: times });
+})
+
 
 
 // Listen on port set in environment variable or default to 3000
